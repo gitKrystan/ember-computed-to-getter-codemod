06794c913d471077fd9064ad0c4d703a2d8f3ec3
Krystan HuffMenne <kmenne+github@gmail.com>
2024-03-21 10:32:53 -0700

    More CLI fixes

--- bin/cli.js
+++ bin/cli.js
@@ -3,28 +3,20 @@ import { Option, program } from 'commander';
 import { globbySync } from 'globby';
 import jscodeshift from 'jscodeshift';
 import fs from 'node:fs';
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import transformer from '../../src/index.js';
-
-// Convert the URL of the current module to a file path.
-const __filename = fileURLToPath(import.meta.url);
-// Use dirname to get the directory path of the current module.
-const __dirname = dirname(__filename);
+import transformer from '../src/index.js';
 
 function run(pathGlob, options) {
   const paths = globbySync(pathGlob);
-  const transformPath = resolve(__dirname, '../src/index.ts');
   if (options.verbose === '2') {
-    console.log('transformPath', transformPath);
     console.log('paths', paths);
     console.log('options', options);
   }
   for (const filePath of paths) {
     console.log('Running transform on file:', filePath);
     const srcCode = fs.readFileSync(filePath, { encoding: 'utf-8' });
-    const collection = jscodeshift(srcCode);
-    const result = transformer(collection, jscodeshift, options);
+    const j = jscodeshift.withParser('ts');
+    const collection = j(srcCode);
+    const result = transformer(collection, { jscodeshift: j }, options);
     fs.writeFileSync(filePath, result, { encoding: 'utf-8' });
   }
   if (options.verbose === '2') {
